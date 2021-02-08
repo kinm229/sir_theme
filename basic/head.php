ad0ed3b2a130a30c6a709dd096c68ccfe4c82551
kinm229 <kinm229@gmail.com>
2021-02-08 16:58:34 +0900

    sda

--- basic/head.php
+++ basic/head.php
@@ -31,14 +31,14 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
         <div id="logo">
             <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_IMG_URL ?>/logo.png" alt="<?php echo $config['cf_title']; ?>"></a>
         </div>
-    
-        
-        <ul class="hd_login">  
-       
 
-            
+
+        <ul class="hd_login">
+
+
+
             <?php if ($is_member) {  ?>
-            
+
             <li><a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=<?php echo G5_BBS_URL ?>/register_form.php">정보수정</a></li>
             <li><a href="<?php echo G5_BBS_URL ?>/logout.php">로그아웃</a></li>
               <?php if ($is_admin) {  ?>
@@ -49,20 +49,20 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 
             <li><a href="<?php echo G5_BBS_URL ?>/register.php">회원가입</a></li>
             <li><a href="<?php echo G5_BBS_URL ?>/login.php">로그인</a></li>
-            
+
             <?php }  ?>
 
         </ul>
     </div>
-    
+
     <nav id="gnb">
         <h2>메인메뉴</h2>
         <div class="gnb_wrap">
             <ul id="gnb_1dul">
                 <li class="gnb_1dli gnb_mnal"><button type="button" class="gnb_menu_btn" title="전체메뉴"><i class="fa fa-bars" aria-hidden="true"></i><span class="sound_only">전체메뉴열기</span></button></li>
                 <?php
-				$menu_datas = get_menu_db(0, true);
-				$gnb_zindex = 999; // gnb_1dli z-index 값 설정용
+                $menu_datas = get_menu_db(0, true);
+                $gnb_zindex = 999; // gnb_1dli z-index 값 설정용
                 $i = 0;
                 foreach( $menu_datas as $row ){
                     if( empty($row) ) continue;
@@ -74,7 +74,7 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
                     $k = 0;
                     foreach( (array) $row['sub'] as $row2 ){
 
-                        if( empty($row2) ) continue; 
+                        if( empty($row2) ) continue;
 
                         if($k == 0)
                             echo '<span class="bg">하위분류</span><div class="gnb_2dul"><ul class="gnb_2dul_box">'.PHP_EOL;
@@ -100,7 +100,7 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
                 <h2>전체메뉴</h2>
                 <ul class="gnb_al_ul">
                     <?php
-                    
+
                     $i = 0;
                     foreach( $menu_datas as $row ){
                     ?>
@@ -135,7 +135,7 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
         </div>
     </nav>
     <script>
-    
+
     $(function(){
         $(".gnb_menu_btn").click(function(){
             $("#gnb_all, #gnb_all_bg").show();
@@ -151,10 +151,35 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 
 
 <hr>
-
+<?if(!defined("_INDEX_")){ ?>
+<div class="subVisual">
+  <div id="page_title" class="sbtImg">
+    <div class="title">
+      <h2 class="loc1D"></h2>
+      <div class="text">안녕하세요</div>
+    </div>
+  </div>
+</div>
+<script>
+  //window.onload = function(){};
+  window.onload = function(){
+    var menuDep = $(".loc1D").html(); //get
+    console.log("현재위치:"+menuDep);
+    if(menuDep == "회사소개"){
+      $(".subVisual .text").text("저희 홈페이지를 찾아주셔서 감사합니다.");
+    }else if(menuDep == "커뮤니티"){
+      $(".subVisual .text").text("자유롭게 커뮤니티하실수 있는 장소입니다.");
+    }else{
+      $(".subVisual .text").text("나이스");
+    }
+  }
+</script>
+<? }?>
 <!-- 콘텐츠 시작 { -->
 <div id="wrapper">
     <div id="container_wr">
-   
-    <div id="container">
-        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
+
+    <div id="container" <?if(defined("_INDEX_")){echo "style=\"width:100%\"";}?>  >
+        <? if (!defined("_INDEX_")) { ?>
+          <h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>">
+          <? echo get_head_title($g5['title']); ?></span></h2><?}?>
